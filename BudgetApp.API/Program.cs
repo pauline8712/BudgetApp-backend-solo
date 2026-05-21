594c7bb7bdd48e68b7d13567f8c7e63c0d7ad517
Pauline <plinekaeorg@gmail.com>
2026-05-21 22:13:13 +0200

    last done

--- BudgetApp.API/Program.cs
+++ BudgetApp.API/Program.cs
@@ -10,7 +10,7 @@ namespace BudgetApp.API
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
 
