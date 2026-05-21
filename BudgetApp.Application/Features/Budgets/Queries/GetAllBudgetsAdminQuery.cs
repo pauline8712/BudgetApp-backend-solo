0daa3c93154156221a1a00373038ecff33eb6b37
Pauline <plinekaeorg@gmail.com>
2026-05-21 19:57:16 +0200

    feat: add GetAllBudgetsAdminQuery for admin access to all budgets

---
+++ BudgetApp.Application/Features/Budgets/Queries/GetAllBudgetsAdminQuery.cs
@@ -0,0 +1,9 @@
+﻿using BudgetApp.Application.Features.Budgets.DTOs;
+using MediatR;
+
+namespace BudgetApp.Application.Features.Budgets.Queries;
+
+// Query för att hämta ALLA budgetar — används bara av Admin
+public class GetAllBudgetsAdminQuery : IRequest<List<BudgetDto>>
+{
+}
