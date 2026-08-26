ef4698aa1a752a6f750af53bd6b1745e8e1b131a
Pauline <plinekaeorg@gmail.com>
2026-08-26 15:10:24 +0200

    Comments for how the BankConnection file is working

---
+++ BudgetApp.Domain/Entities/BankConnection.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//A new database table that are looking att the connection between the user and their bank account for Tink.
+//Because of this class we can see what user has connected with what bank account
+//What acess-token to get the users transactions
+//When the token is expiring
+
+//Its in the Domain with the same reasons as the other classes- Which is "this user have a bankconnectiont). 
+//Busniesslogic
+namespace BudgetApp.Domain.Entities
+{
+    internal class BankConnection
+    {
+    }
+}
