71aec14f52d393fa43fc8b876ba3b30d605d1f41
Pauline <plinekaeorg@gmail.com>
2026-08-28 00:49:36 +0200

    Add ITokenEncryptor interface

---
+++ BudgetApp.Application/Interfaces/ITokenEncryptor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Application-lagret ska inte veta hur krypteringen görs 
+//(det är en Infrastructure-detalj), bara att den finns.
+namespace BudgetApp.Application.Interfaces;
+
+public interface ITokenEncryptor
+{
+    string Encrypt (string plainText);
+    string Decrypt(string cipherText);
+}
