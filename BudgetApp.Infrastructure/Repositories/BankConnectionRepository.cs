df174ebc837bfc489472f2877db620cf440173d5
Pauline <plinekaeorg@gmail.com>
2026-08-27 23:47:29 +0200

    Scaffold BankConnectionRepository class

---
+++ BudgetApp.Infrastructure/Repositories/BankConnectionRepository.cs
@@ -0,0 +1,10 @@
+﻿using BudgetApp.Application.Interfaces;
+using BudgetApp.Domain.Entities;
+using BudgetApp.Infrastructure.Database;
+using Microsoft.EntityFrameworkCore;
+
+namespace BudgetApp.Infrastructure.Repositories;
+
+public class BankConnectionRepository : BaseRepository<BankConnection>, IBankConnectionRepository
+{
+}
