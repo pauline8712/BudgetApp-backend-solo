164f40fb4ee958fd206375124ae871ebeea86e57
Pauline <plinekaeorg@gmail.com>
2026-08-27 23:41:53 +0200

    created a new repository file "IBankConnectionRepository" that is retrieving the same methods from IRepository<T> as Category and Transaction. But I added a own method for BankConnection

---
+++ BudgetApp.Application/Interfaces/IBankConnectionRepository.cs
@@ -0,0 +1,14 @@
+﻿using BudgetApp.Domain.Entities;
+
+//Created this file because so we can actually save and retrieve Bankconnection data from the database via code.
+namespace BudgetApp.Application.Interfaces
+{
+    public interface IBankConnectionRepository : IRepository<BankConnection>
+    {
+        //ärver från en gemensam IRepository<T> som TransactionRep och CategoryRep.
+        //Det ger mig tillgång till grundläggande metoder.
+        //Däremot i den här filen lägger vi till en egen metod GetByUser eftersom vi kommer,
+        //alltid fråga "vilken bankkoppling har den här användaren?"
+        Task<BankConnection?> GetByUserIdAsync(Guid userId);
+    }
+}
