debb0b9dd46cfe37bf0698997b4afd09a0ea84d7
Pauline <plinekaeorg@gmail.com>
2026-08-28 23:14:23 +0200

    Add TestController to test the TokenEncryptor

---
+++ BudgetApp.API/Controllers/TestController.cs
@@ -0,0 +1,11 @@
+﻿using BudgetApp.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+//A temporary testcontroller file to see if the
+//TokenEncryptor is working
+namespace BudgetApp.API.Controllers
+
+{
+    public class TestController
+    {
+    }
+}
