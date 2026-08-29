0510f79b0ed747ccfd0dfdf58aab33c0e79bfea0
Pauline <plinekaeorg@gmail.com>
2026-08-29 16:53:41 +0200

    Add ITinkClient interface

---
+++ BudgetApp.Application/Interfaces/ITinkClient.cs
@@ -0,0 +1,7 @@
+﻿namespace BudgetApp.Application.Interfaces;
+
+public interface ITinkClient
+{
+    string GetAuthorizationUrl(string state);
+    Task<TinkTokenResult> ExchangeCodeForTokenAsync(string code);
+}
