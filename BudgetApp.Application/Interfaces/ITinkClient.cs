caf91d7677d435fba3907f34fd1a62ee398513d0
Pauline <plinekaeorg@gmail.com>
2026-08-29 16:55:52 +0200

    feat: add TinkTokenResult class

--- BudgetApp.Application/Interfaces/ITinkClient.cs
+++ BudgetApp.Application/Interfaces/ITinkClient.cs
@@ -5,3 +5,10 @@ public interface ITinkClient
     string GetAuthorizationUrl(string state);
     Task<TinkTokenResult> ExchangeCodeForTokenAsync(string code);
 }
+
+public class TinkTokenResult
+{
+    public string AccessToken { get; set; } = string.Empty;
+    public string RefreshToken { get; set; } = string.Empty;
+    public int ExpiresIn { get; set; }
+}
