0195373e82e3ae85944aebac79d0dd8d9dfe6877
Pauline <plinekaeorg@gmail.com>
2026-08-29 16:37:56 +0200

    Implement Test method in TestController

--- BudgetApp.API/Controllers/TestController.cs
+++ BudgetApp.API/Controllers/TestController.cs
@@ -11,9 +11,26 @@ public class TestController : ControllerBase
 {
     private readonly ITokenEncryptor _encryptor;
 
-    public TestController (ITokenEncryptor encryptor)
+    public TestController(ITokenEncryptor encryptor)
     {
         _encryptor = encryptor;
+    }
+
+    [HttpGet("encryptor-test")]
+
+    public IActionResult Test()
+    {
+        var original = "hemlig-test-token-123";
+        var encrypted = _encryptor.Encrypt(original);
+        var decrypted = _encryptor.Decrypt(encrypted);
+
+        return Ok(new
+        {
+            original,
+            encrypted,
+            decrypted,
+            matchar = original == decrypted
+        });
     }
 }
 
