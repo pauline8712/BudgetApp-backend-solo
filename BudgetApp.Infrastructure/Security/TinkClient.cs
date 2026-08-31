272b04623f0362d56b127fca1ba2a9b13bbb82b0
Pauline <plinekaeorg@gmail.com>
2026-08-31 12:43:49 +0200

    Scaffold TinkClient with HttpClient and IConfiguration

---
+++ BudgetApp.Infrastructure/Security/TinkClient.cs
@@ -0,0 +1,19 @@
+﻿using BudgetApp.Application.Interfaces;
+using Microsoft.Extensions.Configuration;
+
+namespace BudgetApp.Infrastructure.Security
+{
+    public class TinkClient : ITinkClient
+
+    {
+        private readonly HttpClient _httpClient;
+        private readonly IConfiguration _configuration;
+
+        public TinkClient(HttpClient httpClient, IConfiguration configuration)
+
+        {
+            _httpClient = httpClient;
+            _configuration = configuration;
+        }
+    }
+}
